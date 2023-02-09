784366dd2901b35883e037103e199d5ec7430542
hoangduongle <lehhoangduong@gmail.com>
2023-02-09 13:39:25 +0700

    fix bug

--- src/components/User/useredit.component.jsx
+++ src/components/User/useredit.component.jsx
@@ -8,7 +8,7 @@ function UserEdit({ data, closeModel }) {
             <img
               className="avatar"
               src={
-                "https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/318004607_3295825100734176_5675291227991595834_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=jQKb45OgkLsAX_018gV&tn=vMqjf36MtxNlOrCx&_nc_ht=scontent.fsgn5-8.fna&oh=00_AfDI5VI0Z700m42ejLmTOT19CMUsL_V3Pr0XbkAsVuNeuA&oe=63E48E92"
+                "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
               }
               alt=""
             />
